afdac7252d67adace27960b418f9d56c7c2d3de0
jjh099 <jjh099@icloud.com>
2024-04-03 18:00:00 +0900

    24.04.03 Next.js 생활코딩 13-10 글목록 가져오기

--- 생활코딩/Next.js/실습/start/src/app/page.js
+++ 생활코딩/Next.js/실습/start/src/app/page.js
@@ -1,9 +1,9 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <>
-      <h2></h2>
-    </>
+    <div className="font-bold text-3xl">
+      <h2>Welcome</h2>
+      Hello,WEB!
+      <img src="/hello.png" className="w-52 h-52" />
+    </div>
   );
 }
